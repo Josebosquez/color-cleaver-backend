50267871c083f44ce3282c0060a2f3b5695b9c45
Jose Bosquez <jose.bosquez@codeimmersives.com>
2021-02-25 11:11:23 -0700

    comb

--- color-combinator.js
+++ color-combinator.js
@@ -1,7 +1,15 @@
 /******************
  * YOUR CODE HERE *
  ******************/
-
+function colorCombinator (color, color1){
+  if ((color === "red" || color === "yellow") && (color1 === "red" || color1 === "yellow")){
+    return "orange";
+  } if ((color === "red" || color === "blue") && (color1 === "red" || color1 === "blue")){ 
+    return "purple";
+  } if (color === "yellow" || "blue" && color1 === "yellow" || "blue"){
+    return "green";
+  }
+}
 
 
 
